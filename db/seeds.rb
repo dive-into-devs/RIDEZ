81d3bb7bbd625ded9007016567b3c35253ebea66
Aki Ozawa <ozazachann@gmail.com>
2023-08-23 11:29:13 +0100

    _wig

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create!(email: "ej@ds.com", password: "1232323", full_name: "jack", billing_address: "illinos", phone_number: "00020398239238", payment_information: "Credit Card", driver_license: "938283728")
-Owner.create!(user_id: "1")
+ User.create!(email: "ej@ds.com", password: "1232323", full_name: "jack", billing_address: "illinos", phone_number: "00020398239238", payment_information: "Credit Card", driver_license: "938283728")
+ Owner.create!(user_id: "1")
 Car.create!(plate: "XI234S0", model: "Tesla", vin: "2002292", owner_id: "1", description: "white tesla")
 Offer.create!(user_id: "1", car_id: "1", price: "50$", time_period: "24-11-2023", location: "casablanca")
