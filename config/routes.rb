119c6e0edc3b9f7eebd1dfe99ac6b8c9422c20fc
isaac benlfakih <isaacbenlfakih@gmail.com>
2023-08-24 14:17:27 +0100

    aa

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
   resources :cars do
     resources :offers
   end
